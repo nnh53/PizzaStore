9dc503bd303f6bc0a641b964bab402489c9eaad7
nnh53 <nnh53.work@gmail.com>
2024-10-23 23:07:52 +0700

    CustomerDAO

--- src/java/Model/DAO/CustomerDAO.java
+++ src/java/Model/DAO/CustomerDAO.java
@@ -7,11 +7,9 @@ package Model.DAO;
 
 import Constant.DBMessage;
 import Constant.ErrorMessage;
-import Model.DTO.Account;
 import Model.DTO.Customer;
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.util.ArrayList;
 
 /**
  * hàm nào cũng quăng lỗi hết quăng hết ra ngoài cho Error Handler tổng nhận ***
@@ -67,82 +65,79 @@ public class CustomerDAO {
             throw new Exception(ErrorMessage.ACCOUNT_NOT_EXISTS.enumToString());
         }
         return customerRS;
-    }//end getAccountByAccountID
+    }//end getCustomerByCustomerID
 
-    public Account getAccountByUserName(String searchUserName) throws Exception {
+    public Customer getCustomerByContactName(String searchContactName) throws Exception {
         //1.lấy data từ db
         Connection cn = DBConnection.getConnection();
         ResultSet rs = null;
-        String sql = "SELECT AccountID,UserName,Password,FullName,Type,Status FROM dbo.Account WHERE [UserName]=?";
-        rs = DBConnection.getResultSetFromQuery(cn, sql, searchUserName); //truyền đúng tham số theo sql ko là đi
+        String sql = "SELECT CustomerID,Password,ContactName,Address,Phone,Status FROM dbo.Customer WHERE [ContactName]=?";
+        rs = DBConnection.getResultSetFromQuery(cn, sql, searchContactName); //truyền đúng tham số theo sql ko là đi
         //2.parse/map result
-        Account accountRS = null;
+        Customer customerRS = null;
         if (rs != null && rs.next()) {
-            String accountID = rs.getString(1); //theo lấy 1 2 theo đúng sql
-            String userName = rs.getString(2);
-            String password = rs.getString(3);
-            String fullName = rs.getString(4);
-            String type = rs.getString(5);
+            String customerID = rs.getString(1); //theo lấy 1 2 theo đúng sql
+            String password = rs.getString(2);
+            String contactName = rs.getString(3);
+            String address = rs.getString(4);
+            String phone = rs.getString(5);
             String status = rs.getString(6);
-            accountRS = new Account(accountID, userName, password, fullName, type, status);
+            customerRS = new Customer(customerID, password, contactName, address, phone, status);
             rs.close();
         }
         cn.close();
-        if (accountRS == null) {
+        if (customerRS == null) {
             throw new Exception(ErrorMessage.ACCOUNT_NOT_EXISTS.enumToString());
         }
-        return accountRS;
-    }//end getAccountByUserName
+        return customerRS;
+    }//end getCustomerByContactName
 
     //add thành công trả lại thằng vừa add không thì null hoặc thrown lỗi
-    public Account addAccount(Account accountToAdd) throws Exception {
-        //1.kiểm tra userName có tồn tại chưa
-        Account tmpAccount = this.getAccountByUserName(accountToAdd.getUserName()); //thật ra có thể viết sql để tránh 2 lần connection gọi
-        if (tmpAccount != null) {
-            throw new Exception(ErrorMessage.USERNAME_ALREADY_EXISTS.enumToString());
-        }
+    public Customer addCustomer(Customer customerToAdd) throws Exception {
+        //1.kiểm tra contactName có tồn tại chưa
+        //  contact name cho trùng đc, tên người nhận pizza thôi
         //fix cứng ID tự tăng
-        accountToAdd.setAccountID(this.getLastAccountID());
+        customerToAdd.setCustomerID(this.getLastCustomerID());
         //2.add vô db
         Connection cn = DBConnection.getConnection();
         int af = 0;
-        String sql = "INSERT dbo.Account(AccountID, UserName, Password, FullName, Type, Status)" + "VALUES (\n"
-                + "? ," //-- AccountID - nvarchar(30)\n"
-                + "? ," //-- UserName - nvarchar(30)\n"
-                + "? ," //-- Password - nvarchar(50)\n"
-                + "? ," //-- FullName - nvarchar(50)\n"
-                + "? ," //-- Type - nvarchar(50)\n"
+        String sql = "INSERT dbo.Customer(CustomerID, Password, ContactName, Address, Phone, Status)" + "VALUES (\n"
+                + "? ," //-- CustomerID - nvarchar(30)\n"
+                + "? ," //-- Password - nvarchar(30)\n"
+                + "? ," //-- ContactName - nvarchar(50)\n"
+                + "? ," //-- Address - nvarchar(50)\n"
+                + "? ," //-- Phone - nvarchar(50)\n"
                 + "? ," //-- Status - nvarchar(30)\n"
                 + ");";
-        af = DBConnection.getAffectedRowsFromUpdate(cn, sql, accountToAdd.getAccountID(), accountToAdd.getUserName(), accountToAdd.getPassword(), accountToAdd.getFullName(), accountToAdd.getType(), accountToAdd.getStatus()); //truyền đúng tham số theo sql ko là đi
+        af = DBConnection.getAffectedRowsFromUpdate(cn, sql, customerToAdd.getCustomerID(), customerToAdd.getPassword(), customerToAdd.getContactName(), customerToAdd.getAddress(), customerToAdd.getPhone(), customerToAdd.getStatus()); //truyền đúng tham số theo sql ko là đi
         cn.close();
-        return (af > 0) ? accountToAdd : null; // thành công trả chính nó, ko thì null
-    }//end addAccount
+        return (af > 0) ? customerToAdd : null; // thành công trả chính nó, ko thì null
+    }//end addCustomer
 
     //update thành công trả lại thằng vừa update không thì null hoặc thrown lỗi
-    public Account updateAcccount(Account accountToUpdate) throws Exception {
+    public Customer updateCustomer(Customer customerToUpdate) throws Exception {
         Connection cn = DBConnection.getConnection();
         //1.kiểm tra có tồn tại chưa
-        Account tmpAccount = this.getAccountByAccountID(accountToUpdate.getAccountID()); //thật ra có thể viết sql để tránh 2 lần connection gọi
-        if (tmpAccount != null) {
+        Customer tmpCustomer = this.getCustomerByCustomerID(customerToUpdate.getCustomerID()); //thật ra có thể viết sql để tránh 2 lần connection gọi
+        if (tmpCustomer != null) {
             throw new Exception(ErrorMessage.ACCOUNT_NOT_EXISTS.enumToString());
         }
         //GIỮ LẠI THUỘC TÍNH GÌ CŨ THÌ LẤY LẠI TMP USER XÀI
 
         //2.update vô db
         int af = 0;
-        String sql = "UPDATE dbo.Account SET [AccountID]=?,[UserName]=?,[Password]=?,[FullName]=?,[Type]=?,[Status]=? WHERE AccountID=?"; //CHÚ Ý CÁI WHERE LÀ CÁI CUỐI
-        af = DBConnection.getAffectedRowsFromUpdate(cn, sql, accountToUpdate.getAccountID(), accountToUpdate.getUserName(), accountToUpdate.getPassword(), accountToUpdate.getFullName(), accountToUpdate.getType(), accountToUpdate.getStatus(), accountToUpdate.getAccountID()); //truyền đúng tham số theo sql ko là đi
+        String sql = "UPDATE dbo.Account SET [CustomerID]=?,[Password]=?,[ContactName]=?,[Address]=?,[Phone]=?,[Status]=? WHERE CustomerID=?"; //CHÚ Ý CÁI WHERE LÀ CÁI CUỐI
+        af = DBConnection.getAffectedRowsFromUpdate(cn, sql, customerToUpdate.getCustomerID(), customerToUpdate.getPassword(), customerToUpdate.getContactName(), customerToUpdate.getAddress(), customerToUpdate.getPhone(), customerToUpdate.getStatus(), customerToUpdate.getCustomerID()); //truyền đúng tham số theo sql ko là đi
         cn.close();
-        return (af > 0) ? accountToUpdate : null; // thành công trả chính nó, ko thì null
-    }//end update
+        return (af > 0) ? customerToUpdate : null; // thành công trả chính nó, ko thì null
+    }//end updateCustomer
 
     //delete thành công trả lại thằng vừa delete không thì null hoặc thrown lỗi
-    public Account deleteAccountByAccountId(String idToDelete) throws Exception {
+    public Customer deleteCustomerByCustomerId(String idToDelete) throws Exception {
         Connection cn = DBConnection.getConnection();
         //1.kiểm tra có tồn tại chưa
-        Account tmpAccount = this.getAccountByAccountID(idToDelete); //thật ra có thể viết sql để tránh 2 lần connection gọi
-        if (tmpAccount != null) {
+        Customer tmpCustomer = this.getCustomerByCustomerID(idToDelete); //thật ra có thể viết sql để tránh 2 lần connection gọi
+        if (tmpCustomer != null) {
             throw new Exception(ErrorMessage.ACCOUNT_NOT_EXISTS.enumToString());
         }
         //GIỮ LẠI THUỘC TÍNH GÌ CŨ THÌ LẤY LẠI TMP USER XÀI
@@ -150,66 +145,35 @@ public class CustomerDAO {
         //2.update vô db
         int af = 0;
         String statusMessageDisable = DBMessage.DISSABLED.enumToString();
-        String sql = "UPDATE dbo.Account SET [AccountID]=?,[UserName]=?,[Password]=?,[FullName]=?,[Type]=?,[Status]=? WHERE AccountID=?"; //CHÚ Ý CÁI WHERE LÀ CÁI CUỐI
-        af = DBConnection.getAffectedRowsFromUpdate(cn, sql, tmpAccount.getAccountID(), tmpAccount.getUserName(), tmpAccount.getPassword(), tmpAccount.getFullName(), tmpAccount.getType(), statusMessageDisable, tmpAccount.getAccountID()); //truyền đúng tham số theo sql ko là đi
+        String sql = "UPDATE dbo.Account SET [CustomerID]=?,[Password]=?,[ContactName]=?,[Address]=?,[Phone]=?,[Status]=? WHERE CustomerID=?"; //CHÚ Ý CÁI WHERE LÀ CÁI CUỐI
+        af = DBConnection.getAffectedRowsFromUpdate(cn, sql, tmpCustomer.getCustomerID(), tmpCustomer.getPassword(), tmpCustomer.getContactName(), tmpCustomer.getAddress(), tmpCustomer.getPhone(), statusMessageDisable, tmpCustomer.getCustomerID()); //truyền đúng tham số theo sql ko là đi
         cn.close();
-        return (af > 0) ? tmpAccount : null; // thành công trả chính nó, ko thì null
-    }//end deleteAccountByAccountId
+        return (af > 0) ? tmpCustomer : null; // thành công trả chính nó, ko thì null
+    }//end deleteCustomerByCustomerId
 
-    public Account login(String userNameInp, String passwordInp) throws Exception {
-        //0.kiểm tra userName có tồn tại chưa
-        Account tmpAccount = this.getAccountByUserName(userNameInp);
-        if (tmpAccount == null) {
-            //throw new Exception(ErrorMessage.USERNAME_NOT_EXISTS.enumToString()); -hàm getUserByUserName quăng rồi
-        }
-        //1.lấy data từ db
-        Connection cn = DBConnection.getConnection();
-        ResultSet rs = null;
-        String sql = "SELECT AccountID,UserName,Password,FullName,Type,Status FROM dbo.Account WHERE [UserName]=? AND [Password]=?";
-        rs = DBConnection.getResultSetFromQuery(cn, sql, userNameInp, passwordInp); //truyền đúng tham số theo sql ko là đi
-        //2.parse/map result
-        Account accountRS = null;
-        if (rs != null && rs.next()) {
-            String accountID = rs.getString(1); //theo lấy 1 2 theo đúng sql
-            String userName = rs.getString(2);
-            String password = rs.getString(3);
-            String fullName = rs.getString(4);
-            String type = rs.getString(5);
-            String status = rs.getString(6);
-            accountRS = new Account(accountID, userName, password, fullName, type, status);
-            rs.close();
-        }
-        cn.close();
-        if (accountRS == null) {
-            throw new Exception(ErrorMessage.USERNAME_OR_PASSWORD_INCORRECT.enumToString());
-        }
-        return accountRS;
-    }//end login
-
-    public ArrayList<Account> searchAccountFullName(String searchValue) throws Exception {
-        //data
-        ArrayList<Account> accountListHasFound = new ArrayList<>();
-
-        //1.lấy data từ db
-        Connection cn = DBConnection.getConnection();
-        ResultSet rs = null;
-        String sql = "SELECT AccountID,UserName,Password,FullName,Type,Status FROM dbo.Account WHERE [FullName] LIKE ?";
-        rs = DBConnection.getResultSetFromQuery(cn, sql, searchValue); //truyền đúng tham số theo sql ko là đi
-        //2.parse/map result
-        Account accountRS = null;
-        if (rs != null && rs.next()) {
-            String accountID = rs.getString(1); //theo lấy 1 2 theo đúng sql
-            String userName = rs.getString(2);
-            String password = rs.getString(3);
-            String fullName = rs.getString(4);
-            String type = rs.getString(5);
-            String status = rs.getString(6);
-            accountRS = new Account(accountID, userName, password, fullName, type, status);
-            accountListHasFound.add(accountRS);
-            rs.close();
-        }
-        cn.close();
-        return (accountListHasFound.isEmpty() == true) ? null : accountListHasFound;
-    }//end searchAccountFullName
-
+//    public ArrayList<Account> searchAccountFullName(String searchValue) throws Exception {
+//        //data
+//        ArrayList<Account> accountListHasFound = new ArrayList<>();
+//
+//        //1.lấy data từ db
+//        Connection cn = DBConnection.getConnection();
+//        ResultSet rs = null;
+//        String sql = "SELECT AccountID,UserName,Password,FullName,Type,Status FROM dbo.Account WHERE [FullName] LIKE ?";
+//        rs = DBConnection.getResultSetFromQuery(cn, sql, searchValue); //truyền đúng tham số theo sql ko là đi
+//        //2.parse/map result
+//        Account accountRS = null;
+//        if (rs != null && rs.next()) {
+//            String accountID = rs.getString(1); //theo lấy 1 2 theo đúng sql
+//            String userName = rs.getString(2);
+//            String password = rs.getString(3);
+//            String fullName = rs.getString(4);
+//            String type = rs.getString(5);
+//            String status = rs.getString(6);
+//            accountRS = new Account(accountID, userName, password, fullName, type, status);
+//            accountListHasFound.add(accountRS);
+//            rs.close();
+//        }
+//        cn.close();
+//        return (accountListHasFound.isEmpty() == true) ? null : accountListHasFound;
+//    }//end searchAccountFullName
 }
